fae1d632928109ce5ca4ce35eadf78ef7c2bfb89
mj305 <maria.j.beckles@gmail.com>
2020-09-24 20:57:13 -0400

    header finished

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+import logoWhite from "../Assets/logoWhite.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img className="logo" src={logoWhite} alt="logo" />
+      </div>
+
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub"> is Where Life Happens</span>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
