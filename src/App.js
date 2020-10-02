0937d7972d4dd648ad69d7765b4c6a9401fe9c85
mj305 <maria.j.beckles@gmail.com>
2020-10-01 20:51:32 -0400

    grid not visible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./Components/Header/Header";
-import Content from "./Components/Content/Content";
 
 function App() {
   return (
     <>
       <Header />
-      <Content />
     </>
   );
 }
