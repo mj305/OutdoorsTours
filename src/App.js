77851da25bc67047bc190e399326cd4e69083939
mj305 <maria.j.beckles@gmail.com>
2020-10-01 20:50:40 -0400

    grid finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Header";
+import Content from "./Components/Content/Content";
 
 function App() {
   return (
     <>
       <Header />
+      <Content />
     </>
   );
 }
---
+++ src/Components/Content/Content.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../sass/main.scss";
+
+const Content = () => {
+  return (
+    <div className="grid-test">
+      <div className="row">
+        <div className="col-1-of-2">Column 1 of 2</div>
+        <div className="col-1-of-2">Column 1 of 2</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-3">Column 1 of 3</div>
+        <div className="col-1-of-3">Column 1 of 3</div>
+        <div className="col-1-of-3">Column 1 of 3</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-3">Column 2 of 3</div>
+        <div className="col-2-of-3">Column 2 of 3</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-1-of-4">Column 1 of 4</div>
+        <div className="col-2-of-4">Column 2 of 4</div>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-4 ">Column 1 of 4 </div>
+        <div className="col-3-of-4 ">Column 3 of 4 </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
