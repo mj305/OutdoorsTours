3f365e60d1c66f90dff1a02b44a3bb4ecd1bf8eb
mj305 <maria.j.beckles@gmail.com>
2020-10-07 18:48:51 -0400

    added main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../sass/main.scss";
+
+const Main = () => {
+  return (
+    <div>
+      <h3>Hello From Main</h3>
+      .test
+    </div>
+  );
+};
+
+export default Main;
